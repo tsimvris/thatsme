9e21d58b3ae647db5295a7a4b7efc482cde97452
Tsimvris <tsimvris@gmail.com>
2022-04-09 11:26:09 +0200

    last before open

---
+++ spaß.js
@@ -0,0 +1,15 @@
+if (deadline == 'today') {
+    result = arrange_meeting();
+    if (result == 'success'){
+
+        Play('Call of Duty');
+
+    }
+else{
+    try(ousource());
+    Panik();
+    keepCalm();
+    makeItHappen();
+    play('Call of Duty');
+    }
+}
